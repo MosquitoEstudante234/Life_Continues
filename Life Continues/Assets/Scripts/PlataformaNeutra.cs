c0a20e3480c8264881f124dedddd6a2d5c777442
MosquitoEstudante234 <mosquitoestudante34234@gmail.com>
2023-10-23 16:41:30 -0300

    JoséJeloAgachando
    update dos barco animação, maré.

---
+++ Life Continues/Assets/Scripts/PlataformaNeutra.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaNeutra : MonoBehaviour
+{
+    public Transform Point_A, Point_b;
+    public int speed;
+    Vector2 targetPos;
+
+
+    void Start()
+    {
+        targetPos = Point_b.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  if (Vector2.Distance(transform.position, Point_A.position) < 1f) targetPos = Point_b.position;
+
+        //   if (Vector2.Distance(transform.position, Point_b.position) < 1f) targetPos = Point_A.position;
+
+        //  transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(this.transform);
+            if (Vector2.Distance(transform.position, Point_A.position) < 1f) targetPos = Point_b.position;
+
+            if (Vector2.Distance(transform.position, Point_b.position) < 1f) targetPos = Point_A.position;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+
+        if (collision.CompareTag("Player") && PlayerHealth.iceResistance == true)
+        {
+            collision.transform.SetParent(this.transform);
+            if (Vector2.Distance(transform.position, Point_A.position) < 1f) targetPos = Point_b.position;
+
+            if (Vector2.Distance(transform.position, Point_b.position) < 1f) targetPos = Point_A.position;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+
+}
