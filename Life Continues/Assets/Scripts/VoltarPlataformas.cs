5c92cb17450f9061c663c7aaed5689226d6be607
MosquitoEstudante234 <mosquitoestudante34234@gmail.com>
2023-10-25 15:09:10 -0300

    Meu Deus

--- Life Continues/Assets/Scripts/VoltarPlataformas.cs
+++ Life Continues/Assets/Scripts/VoltarPlataformas.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class VoltarPlataformas : MonoBehaviour
 {
+    public GameObject Plataforma1, Plataforma2, Plataforma3, Plataforma4, Plataforma5, Plataforma6, Plataforma7, Plataforma8;
+    public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +21,18 @@ public class VoltarPlataformas : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Button") && Input.GetKeyDown(KeyCode.E))
+        if (Player.CompareTag("Button") && Input.GetKeyDown(KeyCode.E))
         {
-          
-            
+
+            Plataforma1.transform.position = new Vector2(0, -141);
+            Plataforma2.transform.position = new Vector2(0, -141);
+            Plataforma3.transform.position = new Vector2(0, -141);
+            Plataforma4.transform.position = new Vector2(0, -141);
+            Plataforma5.transform.position = new Vector2(0, -141);
+            Plataforma6.transform.position = new Vector2(0, -141);
+            Plataforma7.transform.position = new Vector2(0, -141);
+            Plataforma8.transform.position = new Vector2(0, -141);
+            Debug.Log("Jose");
         }
 
     }
