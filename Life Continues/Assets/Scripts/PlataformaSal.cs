a3a8d892d2406e7ca8f7f0332c4fd890eb2ae413
MosquitoEstudante234 <mosquitoestudante34234@gmail.com>
2023-10-25 09:30:37 -0300

    nova area de gelo e chechpoints

--- Life Continues/Assets/Scripts/PlataformaSal.cs
+++ Life Continues/Assets/Scripts/PlataformaSal.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlataformaSal : MonoBehaviour
 {
+    public bool EstaGamer;
     public Transform Point_Aum, Point_bum, Point_cum, Point_dum;
     public int speed;
     Vector2 targetPosi;
@@ -11,46 +12,60 @@ public class PlataformaSal : MonoBehaviour
 
     void Start()
     {
-        targetPosi = Point_bum.position;
+        targetPosi = Point_Aum.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //  if (Vector2.Distance(transform.position, Point_A.position) < 1f) targetPos = Point_b.position;
+        if (!EstaGamer)
+        {
+            if (Vector2.Distance(transform.position, Point_Aum.position) < 1f) targetPosi = Point_bum.position;
 
-        //   if (Vector2.Distance(transform.position, Point_b.position) < 1f) targetPos = Point_A.position;
+            if (Vector2.Distance(transform.position, Point_bum.position) < 1f) targetPosi = Point_cum.position;
 
-        //  transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, Point_cum.position) < 1f) targetPosi = Point_dum.position;
 
+            if (Vector2.Distance(transform.position, Point_dum.position) < 1f) targetPosi = Point_Aum.position;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosi, speed * Time.deltaTime);
+
+        }
+      
 
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
 
-        if (collision.CompareTag("Player") && PlayerHealth.fireResistance == true)
+      /*  if (collision.CompareTag("Player"))
         {
             collision.transform.SetParent(this.transform);
             if (Vector2.Distance(transform.position, Point_Aum.position) < 1f) targetPosi = Point_bum.position;
 
-            if (Vector2.Distance(transform.position, Point_bum.position) < 1f) targetPosi = Point_Aum.position;
+            if (Vector2.Distance(transform.position, Point_bum.position) < 1f) targetPosi = Point_cum.position;
 
-            transform.position = Vector2.MoveTowards(transform.position, targetPosi, speed * Time.deltaTime);
-            
-        }
-
-        if (collision.CompareTag("Player") && PlayerHealth.iceResistance == true)
-        {
-            collision.transform.SetParent(this.transform);
             if (Vector2.Distance(transform.position, Point_cum.position) < 1f) targetPosi = Point_dum.position;
 
-            if (Vector2.Distance(transform.position, Point_dum.position) < 1f) targetPosi = Point_cum.position;
+            if (Vector2.Distance(transform.position, Point_dum.position) < 1f) targetPosi = Point_Aum.position;
 
             transform.position = Vector2.MoveTowards(transform.position, targetPosi, speed * Time.deltaTime);
             
         }
 
-    }
+        /*  if (collision.CompareTag("Player") && PlayerHealth.iceResistance == true)
+           {
+               collision.transform.SetParent(this.transform);
+               if (Vector2.Distance(transform.position, Point_cum.position) < 1f) targetPosi = Point_dum.position;
+
+               if (Vector2.Distance(transform.position, Point_dum.position) < 1f) targetPosi = Point_cum.position;
+
+               transform.position = Vector2.MoveTowards(transform.position, targetPosi, speed * Time.deltaTime);
+
+         } */ 
+    
+
+}
+     
 
     private void OnTriggerExit2D(Collider2D collision)
     {
---
+++ Life Continues/Assets/Scripts/PlataformasqueCaem.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformasqueCaem : MonoBehaviour
+{
+    public bool EstaGamer;
+    public Transform Point_Aum, Point_bum;
+    public int speed;
+    Vector2 targetPosi;
+
+
+    void Start()
+    {
+        targetPosi = Point_Aum.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!EstaGamer)
+        {
+            if (Vector2.Distance(transform.position, Point_Aum.position) < 1f) targetPosi = Point_bum.position;
+
+            if (Vector2.Distance(transform.position, Point_bum.position) < 1f) targetPosi = Point_Aum.position;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosi, speed * Time.deltaTime);
+
+        } 
+
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+          if (collision.CompareTag("Player"))
+          {
+             EstaGamer = false;
+            
+          }
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+
+        /*  if (collision.CompareTag("Player") && PlayerHealth.iceResistance == true)
+           {
+               collision.transform.SetParent(this.transform);
+               if (Vector2.Distance(transform.position, Point_cum.position) < 1f) targetPosi = Point_dum.position;
+
+               if (Vector2.Distance(transform.position, Point_dum.position) < 1f) targetPosi = Point_cum.position;
+
+               transform.position = Vector2.MoveTowards(transform.position, targetPosi, speed * Time.deltaTime);
+
+         } */
+
+
+    }
+
+
+   /* private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    } */
+
+}
